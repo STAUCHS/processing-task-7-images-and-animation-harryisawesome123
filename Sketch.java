28c770d6e023950b68b398271f743c8551f04401
harryisawesome123 <harish.rahukulan25@ycdsbk12.ca>
2024-04-16 13:09:21 -0400

    Monkey

--- Sketch.java
+++ Sketch.java
@@ -1,6 +1,26 @@
 import processing.core.PApplet;
+import processing.core.PImage;
+
+/**
+ * 
+ * @author H. Rahukulan
+ *
+ */
 
 public class Sketch extends PApplet {
+
+  PImage imgMonkey;
+
+  // x and y coordinates for the monkey image
+  float fltMonkeyX = 0;
+  float fltMonkeyY = 0;
+
+  // monkey image speed varibles
+  float fltXSpeed = 3;
+  float fltYSpeed = 5;
+
+  // define colours
+  int Black = color(0);
 
   public void settings() {
     size(400, 400);
@@ -8,10 +28,29 @@ public class Sketch extends PApplet {
 
   public void setup() {
     background(210, 255, 173);
+    background(Black);
+    // load meteor
+    imgMonkey = loadImage("monkey-png-25.png");
+    // resize meteor
+    imgMonkey.resize(50, 50);
   }
 
   public void draw() {
-    
+    background(Black);
+
+    // draw meteor and postion
+    image(imgMonkey, fltMonkeyX, fltMonkeyY);
+
+    fltMonkeyX += fltXSpeed;
+    fltMonkeyY += fltYSpeed;
+
+    if (fltMonkeyX > width - imgMonkey.width || fltMonkeyX < 0) {
+      fltXSpeed *= -1;
+      
+    }
+    if (fltMonkeyY > height - imgMonkey.height || fltMonkeyY < 0) {
+      fltYSpeed *= -1;
+    }
   }
 
 }
