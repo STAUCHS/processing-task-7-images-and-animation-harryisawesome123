029bc057b8a6fb30e1ddda698431c8196290a517
harryisawesome123 <harish.rahukulan25@ycdsbk12.ca>
2024-04-17 14:24:54 -0400

    Commited

--- Sketch.java
+++ Sketch.java
@@ -10,47 +10,85 @@ import processing.core.PImage;
 public class Sketch extends PApplet {
 
   PImage imgMonkey;
+  PImage imgBackground;
 
-  // x and y coordinates for the monkey image
-  float fltMonkeyX = 0;
-  float fltMonkeyY = 0;
+  // X and Y coordinates for the monkey image
+  float fltMonkeyX = 40;
+  float fltMonkeyY = 30;
 
   // monkey image speed varibles
-  float fltXSpeed = 3;
-  float fltYSpeed = 5;
+  float fltXMonkeySpeed = 3;
+  float fltYMonkeySpeed = 5;
+
+  // Circle coordinates
+  float fltCircleX = 220;
+  float fltCircleY = 150;
+
+  // Circle image speed variables
+  float fltCircleAngle;
+  float fltCircleSpeed = (float) 0.3;
+
+  // Background coordinates
+  float fltBackgroundX = 0;
+  float fltBackgroundY = 0;
 
   // define colours
   int Black = color(0);
+  int Pink = color(255, 0, 255);
 
   public void settings() {
-    size(400, 400);
+    size(500, 500);
   }
 
   public void setup() {
-    background(210, 255, 173);
     background(Black);
-    // load meteor
+
+    // Load monkey
     imgMonkey = loadImage("monkey-png-25.png");
-    // resize meteor
-    imgMonkey.resize(50, 50);
+
+    // Load Background
+    imgBackground = loadImage("Monkey.png");
+
+    // Resize monkey
+    imgMonkey.resize(100, 100);
+
+    // Resize Background
+    imgBackground.resize(width, height);
   }
 
   public void draw() {
     background(Black);
 
-    // draw meteor and postion
+    // Draw background
+    image(imgBackground, fltBackgroundX, fltBackgroundY);
+
+    // Draw monkey and postion
     image(imgMonkey, fltMonkeyX, fltMonkeyY);
 
-    fltMonkeyX += fltXSpeed;
-    fltMonkeyY += fltYSpeed;
+    fltMonkeyX += fltXMonkeySpeed;
+    fltMonkeyY += fltYMonkeySpeed;
 
     if (fltMonkeyX > width - imgMonkey.width || fltMonkeyX < 0) {
-      fltXSpeed *= -1;
+      fltXMonkeySpeed *= -1;
       
     }
     if (fltMonkeyY > height - imgMonkey.height || fltMonkeyY < 0) {
-      fltYSpeed *= -1;
+      fltYMonkeySpeed *= -1;
     }
+
+    // Draw circle and postion
+    fill(Pink);
+
+    ellipse(fltCircleX, fltCircleY, 50, 50);
+    
+    // Move Circle 
+    fltCircleX += width / 4 * cos(fltCircleAngle) * fltCircleSpeed;
+    fltCircleY += height / 4 * sin(fltCircleAngle) * fltCircleSpeed;
+     
+    // Make circle stay on the screen.
+    fltCircleX = constrain(fltCircleX, 0, width - 25);
+    fltCircleY = constrain(fltCircleY, 0, height - 25);
+    fltCircleAngle += fltCircleSpeed;
   }
 
 }
